7ea476a4572f0c03c25e042912665319bff949a4
Manjil Tamang <lamamanjil@gmail.com>
2023-04-28 21:13:18 +0545

    Fix: instrument boxes hover effect WIP

--- src/components/instruments-box.tsx
+++ src/components/instruments-box.tsx
@@ -28,22 +28,24 @@ const InstrumentsBoxes = ({ instruments }: { instruments: IInstrument[] }) => {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 max-w-[768px] mx-auto pt-10">
         {filteredInstruments.map((instrument, i) => (
-          <Link key={instrument.slug} to={`/instruments/${instrument.slug}`}>
-            <div className="my-8 flex mr-4 hover:scale-105 transition-all	">
-              <div
-                style={{
-                  backgroundColor: `${
-                    instrumentBoxColors[Math.floor(instrument.name.length % instrumentBoxColors.length)] || '#5BB194'
-                  }`,
-                }}
-                className={`shrink-0 w-14 h-14 mt-[6px] text-white font-bold text-center leading-[54px] rounded `}
-              >
-                {getWordInitials(instrument.name)}
-              </div>
-              <div className="pl-4">
-                <p className="font-bold mb-0.5">{instrument.name}</p>
-                <p className="text-slate-500 text-sm leading-5">{instrument.description}</p>
-              </div>
+          <Link
+            key={instrument.slug}
+            to={`/instruments/${instrument.slug}`}
+            className="my-8 flex mr-4 hover:scale-105 transition-all block"
+          >
+            <div
+              style={{
+                backgroundColor: `${
+                  instrumentBoxColors[Math.floor(instrument.name.length % instrumentBoxColors.length)] || '#5BB194'
+                }`,
+              }}
+              className={`shrink-0 w-14 h-14 mt-[6px] text-white font-bold text-center leading-[54px] rounded `}
+            >
+              {getWordInitials(instrument.name)}
+            </div>
+            <div className="pl-4">
+              <p className="font-bold mb-0.5">{instrument.name}</p>
+              <p className="text-slate-500 text-sm leading-5">{instrument.description}</p>
             </div>
           </Link>
         ))}
